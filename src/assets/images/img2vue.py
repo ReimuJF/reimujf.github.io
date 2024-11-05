c4bab0d5c7a2165d5c83f5bd1e78cd4c797de2f6
ReimuJF <slavalang@gmail.com>
2024-11-05 20:22:42 +0500

    deleted utility python file

--- src/assets/images/img2vue.py
+++
@@ -1,8 +0,0 @@
-import glob
-
-files =  glob.glob('*.svg')
-for file in files:
-    with open(file, 'r') as frd, open(f'{file.replace('.svg', '').capitalize()}Component.vue', 'w') as fwrt:
-            fwrt.write(f'<template>{frd.read()}</template>')
-
-input()
