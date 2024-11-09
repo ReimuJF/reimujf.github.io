f1785ca990fe1146a7650e528aa4654e24fa6cfb
ReimuJF <slavalang@gmail.com>
2024-11-09 10:06:02 +0500

    add page not found route, but it seems not to work on gh-pages

--- src/scipts/router.js
+++ src/scipts/router.js
@@ -1,13 +1,14 @@
 import { createWebHistory, createRouter } from 'vue-router'
 
-import HomeView from '../views/HomePage.vue'
-import ContactsView from '../views/ContactsPage.vue'
-import ProjectView from '@/views/ProjectPage.vue'
+import HomeView from '../views/HomePage.vue';
+import ContactsView from '../views/ContactsPage.vue';
+import ProjectView from '@/views/ProjectPage.vue';
+import NotFound from '@/views/NotFoundView.vue';
 const routes = [
-  { path: '', component: HomeView },
   { path: '/', component: HomeView, props: true },
   { path: '/project/:id', name: "Project", component: ProjectView, props: true },
-  { path: '/contacts', name: "contacts", component: ContactsView, props: true },
+  { path: '/contacts', name: "Contacts", component: ContactsView, props: true },
+  {path: "/:pathMatch(.*)*", name: "Not Found", component: NotFound }
 ]
 
 const router = createRouter({
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="project-container max-width">
+        <h2 class="largest-font-size margin-bottom40" >Page Not Found</h2>
+   <router-link to="/"><button class="button big-font-size">To Main Page</button></router-link>
+
+    </div>
+</template>
+
+<style scoped>
+.project-container {
+    height: 90vh;
+    padding-bottom: 30px;
+    margin: 40px auto 40px auto;
+    background: rgba(107, 109, 164, 0.36);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(11px);
+    -webkit-backdrop-filter: blur(11px);
+    border: 1px solid rgba(107, 109, 164, 0.41);
+    text-align: center;
+}
+
+</style>
